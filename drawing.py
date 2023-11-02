225cb0679c7e0ae94fd745ee49337cad80ba4757
Eugene T <tyanbz@gmail.com>
2023-11-02 10:47:18 +0600

    init

---
+++ drawing.py
@@ -0,0 +1,15 @@
+import turtle as t
+
+t.color('green')
+t.shape('triangle')
+t.pensize(1)
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+t.exitonclick()
